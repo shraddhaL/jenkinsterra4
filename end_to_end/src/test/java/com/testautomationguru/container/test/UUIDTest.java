54710491135375b25d8b6523902c9de5ca8ac362
shraddhaL <52315608+shraddhaL@users.noreply.github.com>
2021-02-03 12:53:02 +0530

    Update UUIDTest.java

--- end_to_end/src/test/java/com/testautomationguru/container/test/UUIDTest.java
+++ end_to_end/src/test/java/com/testautomationguru/container/test/UUIDTest.java
@@ -6,7 +6,6 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import java.net.MalformedURLException;
-import com.testautomationguru.container.pages.SearchPage;
 import com.google.common.util.concurrent.Uninterruptibles;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
