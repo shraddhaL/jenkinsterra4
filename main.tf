c7b3cfca73668ee029f1d3bcc465511efb2cac22
shraddhaL <52315608+shraddhaL@users.noreply.github.com>
2021-02-04 00:26:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,16 @@
 
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "2.11.0"
+    }
+  }
+}
 
 resource "docker_container" "tomcat_container" {
   name  = "mytomcat"
-  image = docker_image.tomcat_image.name
+  image = docker_image.tomcat_image.latest
   ports = {
     internal = "8080"
     external = "9090"
