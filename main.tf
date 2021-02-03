f2e1b45e5855e67e9ce63f49f0e2f03141cb9fe7
shraddhaL <52315608+shraddhaL@users.noreply.github.com>
2021-02-03 22:33:54 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "docker" {}
+
+resource "docker_container" "tomcat_container" {
+  name  = "mytomcat"
+  image = docker_image.tomcat_image.latest
+  ports = {
+    internal = "8080"
+    external = "9090"
+  }
+}
+
+resource "docker_image" "tomcat_image" {
+  name = "shraddhal/tomcat_develop:latest"
+}
