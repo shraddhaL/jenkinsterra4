3ae3199f9a5ec02a2ff48b125512d30af56eee1d
shraddhaL <52315608+shraddhaL@users.noreply.github.com>
2021-02-04 15:09:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "aws_instance" "web" {
     inline = [
       "cd /end_to_end",
       "sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose",
-  "sudo chmod +x /usr/local/bin/docker-compose"'
+  "sudo chmod +x /usr/local/bin/docker-compose",
 "docker-compose --version",
       "docker-compose up -d --scale chrome=3",
     ]
