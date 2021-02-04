46e90d91303b308e9764b7b83ed314b7b0417f9a
shraddhaL <52315608+shraddhaL@users.noreply.github.com>
2021-02-04 15:08:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,31 +35,26 @@ resource "aws_instance" "web" {
     instance_type = "t2.micro"
    key_name = "azureaws"
    
-  tags = {
-    Name = "remote-exec-provisioner"
-  }
-}
-
-
-resource "null_resource" "copy_execute" {
-  
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      host        = aws_instance.web.public_ip
-      private_key = file("azureaws.pem")
-    }
  provisioner "file" {
     source      = "end_to_end/docker-compose.yml"
     destination = "/end_to_end/docker-compose.yml"
   } 
    provisioner "remote-exec" {
+      connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      host        = aws_instance.web.public_ip
+      private_key = file("azureaws.pem")
+    }
     inline = [
       "cd /end_to_end",
+      "sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose",
+  "sudo chmod +x /usr/local/bin/docker-compose"'
+"docker-compose --version",
       "docker-compose up -d --scale chrome=3",
     ]
   }
   
-  depends_on = [ aws_instance.web ]
+ 
   
   }
