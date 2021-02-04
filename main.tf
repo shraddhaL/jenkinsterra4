046a0fe8621bf5b229e03b824ed75b9bf3a8c554
shraddhaL <52315608+shraddhaL@users.noreply.github.com>
2021-02-04 16:31:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -109,6 +109,7 @@ provisioner "file" {
     inline = [
       "sudo chmod 777 /end_to_end",
       "cd /end_to_end",
+      "sudo chmod 777 /end_to_end/docker-compose.yml",
       "sudo curl -L https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
       "sudo chmod +x /usr/local/bin/docker-compose",
       "docker-compose --version",
