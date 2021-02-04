bbcffceb00f586fd2ba5a943e1e8057e465a6886
shraddhaL <52315608+shraddhaL@users.noreply.github.com>
2021-02-04 21:04:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,18 +23,12 @@ resource "docker_image" "tomcat_image" {
 }
 
 resource "null_resource" "composedocker" {
-  provisioner "local-exec" {
-    command = "pwd"
-  }
-   provisioner "local-exec" {
-    command = "cd end_to_end"
-  }
-  provisioner "local-exec" {
-    command = "pwd"
-  }
-  provisioner "local-exec" {
-    command = "docker-compose up -d --scale chrome=3"
-  }
   
+   command = <<EOT
+     pwd
+     cd end_to_end
+      pwd
+    docker-compose up -d --scale chrome=3
+   EOT
   
 }
