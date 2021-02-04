b755d12409b0fbdf9daf2309e7e449902102863b
shraddhaL <52315608+shraddhaL@users.noreply.github.com>
2021-02-04 11:16:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ terraform {
       source = "kreuzwerker/docker"
       version = "2.11.0"
     }
-     digitalocean = {
-      source = "digitalocean/digitalocean"
-      version = "2.5.0"
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.26.0"
     }
   }
 }
@@ -25,12 +25,27 @@ resource "docker_image" "tomcat_image" {
   name = "shraddhal/tomcat_develop"
 }
 
-resource "digitalocean_droplet" "web" {
 
-  image  = "ubuntu-18-04-x64"
-  name   = "web-1"
-  region = "nyc2"
-  size   = "s-1vcpu-1gb"
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+}
+
+resource "aws_instance" "web" {
+  ami = data.aws_ami.ubuntu.id
+    instance_type = "t3.micro"
+  
   provisioner "file" {
     source      = "end_to_end/docker-compose.yml"
     destination = "/end_to_end"
