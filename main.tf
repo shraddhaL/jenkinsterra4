b2131529135707a3924b4d126e062d05fa9ab075
shraddhaL <52315608+shraddhaL@users.noreply.github.com>
2021-02-04 16:20:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -113,9 +113,10 @@ provisioner "file" {
       private_key = file("azureaws.pem")
     }
     inline = [
-      "cd /end_to_end",
       "sudo curl -L https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
       "sudo chmod +x /usr/local/bin/docker-compose",
+      "sudo chmod 777 /end_to_end",
+      "cd /end_to_end",
       "docker-compose --version",
       "docker-compose up -d --scale chrome=3",
     ]
