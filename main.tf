680e6360c5a60bd99c428390554851710b276f73
shraddhaL <52315608+shraddhaL@users.noreply.github.com>
2021-02-04 20:43:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,16 +5,8 @@ terraform {
       source = "kreuzwerker/docker"
       version = "2.11.0"
     }
-    aws = {
-      source = "hashicorp/aws"
-      version = "3.26.0"
-    }
+   
   }
-}
-provider "aws" {
-   access_key = "AKIAUKBXP6VH4WUSR6GX"
-   secret_key = "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"
-   region = "us-east-2"
 }
 
 resource "docker_container" "tomcat_container" {
@@ -30,123 +22,9 @@ resource "docker_image" "tomcat_image" {
   name = "shraddhal/tomcat_develop"
 }
 
-resource "aws_instance" "web" {
-  ami = "ami-01aab85a5e4a5a0fe" 
-    instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.webSG.id]
-   key_name = "azureaws"
-   associate_public_ip_address = true
-   tags = {
-    Name = "remote-exec-provisioner"
+resource "null_resource" "composedocker" {
+  provisioner "local-exec" {
+    command = "docker-compose up -d --scale chrome=3"
+   
   }
 }
-
-resource "aws_security_group" "webSG" {
-  name        = "webSG"
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8081
-    to_port     = 8081
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 5900
-    to_port     = 5900
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port = 4444
-    to_port   = 4444
-    protocol  = "tcp"
-
-    # To keep this example simple, we allow incoming SSH requests from any IP. In real-world usage, you should only
-    # allow SSH requests from trusted servers, such as a bastion host or VPN server.
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "null_resource" "copy_execute" {
-  triggers = {
-    public_ip = aws_instance.web.public_ip
-  }
-
-  connection {
-    type  = "ssh"
-    host  = aws_instance.web.public_ip
-    user        = "ec2-user"
-    private_key = file("azureaws.pem")
-  }
-  
-  
-  
-    provisioner "remote-exec" {
-      connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      host        = aws_instance.web.public_ip 
-      private_key = file("azureaws.pem")
-    }
-    inline = [
-      "sudo mkdir -p /end_to_end",
-      "sudo chmod 777 /end_to_end",
-      ]
-  }
-  
-  
-
-  // copy our example script to the server
-provisioner "file" {
-    source      = "end_to_end/docker-compose.yml"
-    destination = "/end_to_end/docker-compose.yml"
-  
-   connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      host        = aws_instance.web.public_ip 
-      private_key = file("azureaws.pem")
-    }
-  } 
-
-  // change permissions to executable and pipe its output into a new file
-  provisioner "remote-exec" {
-      connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      host        = aws_instance.web.public_ip 
-      private_key = file("azureaws.pem")
-    }
-    inline = [
-      "sudo chmod 777 /end_to_end",
-      "sudo chmod 777 /end_to_end/docker-compose.yml",
-      "cd /end_to_end",
-      "sudo yum update -y",
-      "sudo yum install docker -y",
-      "sudo service docker start",
-      "sudo docker --version",
-      "sudo usermod -a -G docker ec2-user",
-      "sudo chmod 666 /var/run/docker.sock",
-      "sudo curl -L https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
-      "sudo chmod +x /usr/local/bin/docker-compose",
-      "docker-compose --version",
-      "docker-compose up -d --scale chrome=3",
-    ]
-  }
-  
-    depends_on = [ aws_instance.web ]
-}
-
