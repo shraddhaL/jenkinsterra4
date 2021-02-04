5daded01d4fd38bfc052960a46751f59b8b38de3
shraddhaL <52315608+shraddhaL@users.noreply.github.com>
2021-02-04 12:32:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,15 +6,16 @@ terraform {
       version = "2.11.0"
     }
     aws = {
-     # source = "hashicorp/aws"
-     # version = "3.26.0"
-      access_key = "AKIAUKBXP6VHTML56ZNF"
-   secret_key = "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"
-   region = "us-east-2"
+      source = "hashicorp/aws"
+      version = "3.26.0"
     }
   }
 }
-
+provider "aws" {
+   access_key = "AKIAUKBXP6VHTML56ZNF"
+   secret_key = "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"
+   region = "us-east-2"
+}
 resource "docker_container" "tomcat_container" {
   name  = "mytomcat"
   image = docker_image.tomcat_image.latest
