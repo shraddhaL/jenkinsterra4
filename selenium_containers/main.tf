a88175db3c09f7bc2f53198cd01a5b731a4f42d4
shraddhaL <52315608+shraddhaL@users.noreply.github.com>
2021-02-05 15:28:17 +0530

    Update main.tf

--- selenium_containers/main.tf
+++ selenium_containers/main.tf
@@ -1,16 +1,11 @@
-terraform {
-  required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
-      version = "2.11.0"
-    }
-  }
-}
-
-
 resource "null_resource" "composedocker" {
   provisioner "local-exec" {
        command = "docker-compose up -d --scale chrome=3"
   }
 }
 
+resource "null_resource" "composedocker" {
+  provisioner "local-exec" {
+       command = "docker-compose down"
+  }
+}
