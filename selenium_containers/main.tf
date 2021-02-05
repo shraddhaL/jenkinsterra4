38f3ceb182ef744d4f0c46d928c3e58bd7638d4e
shraddhaL <52315608+shraddhaL@users.noreply.github.com>
2021-02-05 15:33:54 +0530

    Update main.tf

--- selenium_containers/main.tf
+++ selenium_containers/main.tf
@@ -1,10 +1,10 @@
-resource "null_resource" "composedocker" {
+resource "null_resource" "containers_up" {
   provisioner "local-exec" {
        command = "docker-compose up -d --scale chrome=3"
   }
 }
 
-resource "null_resource" "composedocker" {
+resource "null_resource" "containers_down" {
   provisioner "local-exec" {
        command = "docker-compose down"
   }
