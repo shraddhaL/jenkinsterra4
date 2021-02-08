f8eea9c568413efcaf837e3dd948caa3b4ec75ac
shraddhaL <52315608+shraddhaL@users.noreply.github.com>
2021-02-08 13:41:59 +0530

    Update install_tomcat.sh

--- aws_tomcat/install_tomcat.sh
+++ aws_tomcat/install_tomcat.sh
@@ -5,3 +5,6 @@ sudo systemctl start tomcat
 cd /usr/share/tomcat/webapps/
 sudo chmod 777 /usr/share/tomcat/
 sudo chmod 777 /usr/share/tomcat/webapps
+sudo groupadd tomcat
+sudo chown -R :tomcat /opt/tomcat/webapps
+sudo chmod g+w /opt/tomcat/webapps
