e8a5cd81b898c4c64e73e8cef465d75fcf9ec61f
shraddhaL <52315608+shraddhaL@users.noreply.github.com>
2021-02-08 12:46:18 +0530

    Update install_tomcat.sh

--- aws_tomcat/install_tomcat.sh
+++ aws_tomcat/install_tomcat.sh
@@ -2,6 +2,6 @@
 sudo amazon-linux-extras install tomcat8.5 -y
 sudo systemctl enable tomcat
 sudo systemctl start tomcat
-cd /usr/share/tomcat/webapps/
 sudo chmod 777 /usr/share/tomcat/
 sudo chmod 777 /usr/share/tomcat/webapps
+cd /usr/share/tomcat/webapps/
