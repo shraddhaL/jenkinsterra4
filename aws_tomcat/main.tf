7f429947f881286b290551f11820a34ed3a62e3f
shraddhaL <52315608+shraddhaL@users.noreply.github.com>
2021-02-09 22:40:22 +0530

    Update main.tf

--- aws_tomcat/main.tf
+++ aws_tomcat/main.tf
@@ -7,19 +7,24 @@ terraform {
   }
 }
 provider "aws" {
-   access_key = ""
-   secret_key = ""
+   access_key = var.access
+   secret_key = var.secret
    region = "us-east-2"
+}
+
+resource "aws_key_pair" "my_key" {
+  key_name   = "mykey"
+  public_key = file(var.public_key)
 }
 resource "aws_instance" "web" {
   ami = "ami-01aab85a5e4a5a0fe" 
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.webSG2.id]
-  key_name = "azureaws"
- 
+  key_name = aws_key_pair.my_key.key_name
+  user_data = data.template_file.asg_init.rendered
   associate_public_ip_address = true
   tags = {
-    Name = "remote-exec-provisioner"
+    Name = "deploy-on-aws"
   }
 }
 
@@ -45,7 +50,7 @@ ingress {
   }
 }
 
-resource "null_resource" "copy_execute" {
+resource "null_resource" "copy_file" {
   triggers = {
     public_ip = aws_instance.web.public_ip
   }
@@ -54,40 +59,45 @@ resource "null_resource" "copy_execute" {
     type  = "ssh"
     host  = aws_instance.web.public_ip
     user        = "ec2-user"
-    private_key = file("azureaws.pem")
+    private_key =file(var.private_key)
   }
 
-  provisioner "remote-exec" {
-      connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      host        = aws_instance.web.public_ip 
-      private_key = file("azureaws.pem")
-    }
-    inline = [
-        "sudo amazon-linux-extras install tomcat8.5 -y",
-        "sudo systemctl enable tomcat",
-        "sudo systemctl start tomcat",
-        "cd /usr/share/tomcat/webapps/",
-        "sudo chmod 777 /usr/share/tomcat/",
-        "sudo chmod 777 /usr/share/tomcat/webapps",
-    ]
-  }
-    provisioner "file" {
+provisioner "file" {
     source      = "/opt/tomcat/tomcat9/webapps/roshambo.war"
-    destination = "/usr/share/tomcat/webapps/roshambo.war"
-  
-   connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      host        = aws_instance.web.public_ip 
-      private_key = file("azureaws.pem")
+      destination = "/tmp/roshambo.war"
+      connection {
+        type     = "ssh"
+        user     = "ec2-user"
+        host     = aws_instance.web.public_ip 
+        private_key =file(var.private_key)
+      }
     }
-  } 
+  
     depends_on = [ aws_instance.web ]
 }
 
+
+
+
 output "DNS" {
   value = aws_instance.web.public_ip
+  value=format("Access the AWS hosted webapp from here: http://%s%s", aws_instance.web.public_dns, ":8080/roshambo")
+}
 
+data "template_file" "asg_init" {
+  template = file("${path.module}/userdata.tpl")
+}
+variable "access" {
+  type = string
+}
+variable "secret" {
+  type = string
+}
+variable "private_key" {
+  type = string
+}
+
+
+variable "public_key" {
+  type = string
 }
