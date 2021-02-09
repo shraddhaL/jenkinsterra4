f94e01bda780c3d01e54d7cd9570f856df8b0d1b
shraddhaL <52315608+shraddhaL@users.noreply.github.com>
2021-02-10 02:22:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,14 +10,14 @@ variable "secret" {
 variable "access" {
   type = string
 }
-
+/*
 module "tomcat_container" {
   source = "./tomcat_container"
 }
 
  module "selenium_containers" {
   source = "./selenium_containers"
-} 
+} */
 provider "aws" {
    access_key = var.access
    secret_key = var.secret
