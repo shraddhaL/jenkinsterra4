ee09771e5613707790d78b680e5d14568117fec3
shraddhaL <52315608+shraddhaL@users.noreply.github.com>
2021-02-10 00:10:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,9 +6,22 @@ module "tomcat_container" {
  module "selenium_containers" {
   source = "./selenium_containers"
 } 
-   
+
+variable "access" {
+  type = string
+}
+variable "secret" {
+  type = string
+}
+variable "private_key" {
+  type = string
+}
+
+
  module "aws_tomcat" {
   source = "./aws_tomcat"
-   
+     access=var.access 
+   secret=var.secret
+   private_key=var.private_key
 }   
 
